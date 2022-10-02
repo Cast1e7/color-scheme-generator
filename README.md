5e8883df861c3642d27c0d428559cf0ce3ca8d9f
Shailesh Upadhyay <shaileshu257@gmail.com>
2022-10-02 23:51:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Color-Scheme Generator App
+
+This is a solution to the [Scrimba Color-Scheme Generator Solo Project](https://scrimba.com/allcourses).
+
+## Table of contents
+
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Usage
+
+- The site generates a color scheme using the color api.
+- Loads with default values.Select the color,option from the dropdown and submit the form to get color scheme of choice.
+- The hex code for any of the rendered colors can be copied by either clicking on the color or the   hex code below.
+
+## Screenshot
+
+![](./screenshots/img-daymode.png)
+
+## Links
+
+- Live Site URL: [tbd](tbd)
+- Color Api: 
+
+
+## Built with
+
+- HTML5 markup
+- Pure CSS
+- Vanilla JavaScript
+- Flexbox
+- thecolorapi
+
+
+## Author
+
+- GitHub - [Shailesh Upadhyay](https://github.com/Cast1e7)
+
+## Acknowledgments
+
+This solo project is part of the frontend course by Scrimba. 
+You can also learn from Scrimba. Check the Links below.
+
+- [Scrimba courses](https://scrimba.com/allcourses)
+- [The Frontend Career Path](https://scrimba.com/learn/frontend)
+- [Become a Scrimba Pro member](https://scrimba.com/pricing)
+
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,60 @@
+const colorOuput = document.getElementById("color-output")
+const seedColor = document.getElementById("seed-color")
+const colorMode = document.getElementById("color-mode")
+const colorPicker = document.getElementById("color-picker")
+let isWaiting = false
+let defaultColor = "f55A5A"
+let defaultMode = "monochrome"
+seedColor.value = "#".concat(defaultColor)
+
+
+getColorScheme(defaultColor, defaultMode)
+
+colorPicker.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const selectedMode = colorMode.value
+    const selectedColor = seedColor.value.slice(1)
+    getColorScheme(selectedColor,selectedMode)
+})
+
+
+
+function getColorScheme(color, mode) {
+  fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`, {method: "GET"}).then(res => res.json())
+       .then(data =>  {
+            const colorDataArr = data.colors
+            renderColors(colorDataArr)
+            })  
+}
+
+
+function renderColors(dataArr) {
+    colorOuput.innerHTML = dataArr.map(color => {
+           return `<div class="scheme">
+                        <div class="scheme-color hex-color" style="background-color:${color.hex.value}">${color.hex.value}</div>
+                        <p class="scheme-hex-code hex-color" id=${color.hex.value}>${color.hex.value}</p>
+                  </div>`
+            }).join("")
+}
+
+
+colorOuput.addEventListener("click", function(e) {
+    if(!isWaiting) {
+    const hexMatches = e.target.matches('.hex-color')
+    const hex = e.target
+    const colorHexValue = hex.textContent
+    const hexValueDom = document.getElementById(colorHexValue)
+    console.log(hex.textContent)
+    // navigator.clipboard.writeText(hex.textContent)
+    hexValueDom.textContent = "Hex Copied"
+    hexValueDom.style.background = "#000000"
+    hexValueDom.style.color = "#ffffff"
+    isWaiting = true
+    setTimeout(function() {
+        hexValueDom.textContent = colorHexValue
+        hexValueDom.style.background = "#ffffff"
+        hexValueDom.style.color = "#000000"
+        isWaiting = false
+    }, 1200) 
+    }
+})
